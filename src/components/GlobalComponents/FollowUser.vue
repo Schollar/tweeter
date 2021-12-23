23e73850e703eedd7129229cb24ed20687248d18
schollar <cschollar22@gmail.com>
2021-12-22 18:19:13 -0700

    Added ability to follow and unfollow users, button changes based on if user is followed or not

--- src/components/GlobalComponents/FollowUser.vue
+++ src/components/GlobalComponents/FollowUser.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
+  <div v-if="is_followed">
+    <button @click="unfollow_user()">UnFollow user</button>
+    <p>{{ api_message }}</p>
+  </div>
+  <div v-else>
     <button @click="follow_user()">Follow user</button>
     <p>{{ api_message }}</p>
   </div>
@@ -14,26 +18,66 @@ export default {
   data() {
     return {
       api_message: "",
+      user: this.$cookies.get("user"),
+      followed_users: [],
+      is_followed: false,
     };
-  },
-  computed: {
-    user() {
-      return this.$store.state.user;
-    },
   },
   props: {
     selected_user: Object,
   },
   methods: {
-    get_loggedin_user_follows() {},
+    unfollow_user() {
+      this.$axios
+        .request({
+          url: "https://tweeterest.ga/api/follows",
+          method: "DELETE",
+          data: {
+            loginToken: this.user.loginToken,
+            followId: this.selected_user.userId,
+          },
+        })
+        .then((response) => {
+          response;
+          this.api_message = `You are no longer following ${this.selected_user.username}`;
+          this.is_followed = false;
+        })
+        .catch((error) => {
+          error;
+          this.api_message = `Sorry something went wrong, please try again later`;
+        });
+    },
+    check_user_followed() {
+      for (var i = 0; i < this.followed_users.length; i++) {
+        if (this.selected_user.userId === this.followed_users[i].userId) {
+          this.is_followed = true;
+        } else {
+          this.is_followed = false;
+        }
+      }
+    },
+    get_loggedin_user_follows() {
+      this.$axios
+        .request({
+          url: "https://tweeterest.ga/api/follows",
+          method: "GET",
+          params: {
+            userId: this.user.userId,
+          },
+        })
+        .then((response) => {
+          this.followed_users = response.data;
+          this.check_user_followed();
+        })
+        .catch((error) => {
+          error;
+          this.api_message = `Sorry something went wrong, please try again later`;
+        });
+    },
     move_to_user_profile() {
       this.$router.push({ path: "/user-profile" });
     },
     follow_user() {
-      var logintoken = this.user.loginToken;
-      var followid = this.selected_user.userId;
-      console.log(logintoken);
-      console.log(followid);
       this.$axios
         .request({
           url: "https://tweeterest.ga/api/follows",
@@ -46,6 +90,7 @@ export default {
         .then((response) => {
           response;
           this.api_message = `You are now following ${this.selected_user.username}`;
+          this.is_followed = true;
         })
         .catch((error) => {
           error;
